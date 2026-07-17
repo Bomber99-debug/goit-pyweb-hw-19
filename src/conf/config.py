8edee603733a9bcd8291aab453eb64edb1f312e4
Bomber99-debug <68162111+Bomber99-debug@users.noreply.github.com>
2026-07-17 18:53:54 +0300

    add migrations

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,5 +1,4 @@
-from conect_setings import user, db_name, password, host, port
-
+from src.conf.connect_settings import user, db_name, password, host, port
 
 class Config:
 	DB_URL = f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{db_name}"
